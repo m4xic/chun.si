2e5401890d3ec589ad1dda8fc8afd9794606137d
Max <m@xic.email>
2021-06-30 19:08:59 +0100

    Replace disabled message on comments: false posts with global message

--- src/components/Post/Comments/Comments.js
+++ src/components/Post/Comments/Comments.js
@@ -8,10 +8,14 @@ type Props = {
 const Comments = ({ comments }: Props) => {
 
   if (!comments) {
+    return (<p>( Comments are currently disabled on <b>chun.si</b>. )</p>);
+  }
+
+  if (!comments) {
     return (<p>( Comments are disabled on this post. )</p>);
   }
 
-  return (<p>( Comments are currently disabled on <b>chun.si</b>. )</p>)
+  return (<p>( Comments are currently disabled on <b>chun.si</b>. )</p>);
 
   return (
     <script src="https://utteranc.es/client.js"
