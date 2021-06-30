23cfb8c6f2cbc57238e87e82ffd6239c1167cc3b
Max <m@xic.email>
2021-06-30 23:28:04 +0100

    Close tags on <link> and <meta> in head

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -34,13 +34,13 @@ const Layout = ({
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={metaImageUrl} />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#e0d6ff">
-        <meta name="msapplication-TileColor" content="#e0d6ff">
-        <meta name="theme-color" content="#ffffff">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#e0d6ff" />
+        <meta name="msapplication-TileColor" content="#e0d6ff" />
+        <meta name="theme-color" content="#ffffff" />
       </Helmet>
       {children}
     </div>
