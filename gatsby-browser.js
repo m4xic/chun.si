4c2d44858be7410b2520eb7a614ffd0f6cd3dc72
Max <m@xic.email>
2021-06-30 19:47:23 +0100

    Add littlefoot

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,10 @@
 
 require('./src/assets/scss/init.scss');
 require('./static/css/prismjs/theme.min.css');
+
+import littlefoot from 'littlefoot'
+import 'littlefoot/dist/littlefoot.css'
+
+export function onRouteUpdate() {
+  littlefoot() // Pass any littlefoot settings here.
+}
