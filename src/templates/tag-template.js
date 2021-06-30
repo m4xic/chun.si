a720eb5f1d98aebef8ead7a04a8f2b1c8574a122
Max <m@xic.email>
2021-06-30 18:09:46 +0100

    Fix capitalisation on tag post title

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -27,7 +27,7 @@ const TagTemplate = ({ data, pageContext }: Props) => {
   } = pageContext;
 
   const { edges } = data.allMarkdownRemark;
-  const pageTitle = currentPage > 0 ? `All posts tagged as ${tag}: Page ${currentPage} • ${siteTitle}` : `All Posts tagged as "${tag}" • ${siteTitle}`;
+  const pageTitle = currentPage > 0 ? `All posts tagged as ${tag}: Page ${currentPage} • ${siteTitle}` : `All posts tagged as "${tag}" • ${siteTitle}`;
 
   return (
     <Layout title={pageTitle} description={siteSubtitle}>
